f6a1187b8ce045562302612bcb9d89535be4eb51
Hrutiksha <hrutiksha.xworkz@gmail.com>
2024-07-17 23:53:50 +0100

    classes and objects

---
+++ Task 13/PenRunner.java
@@ -0,0 +1,18 @@
+class PenRunner{
+
+public static void main(String[] factory){
+
+    //className ref = new ClassName();
+	Pen ref = new Pen();
+	ref.name = "Renolds";
+	ref.id =1;
+	ref.color = "Red";
+	ref.price = 10.00;
+	
+	System.out.println("the brand name is"+ref.name);
+	System.out.println("The id id"+ref.id);
+	System.out.println("The color is"+ref.color);
+	System.out.println("The price is"+ref.price);
+	}
+	
+	}
